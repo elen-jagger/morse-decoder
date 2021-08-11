6dfe478173d994ba7aef05940fdcdbffb9c68367
Elena Leusik <elena.leusik@gmail.com>
2021-08-11 18:33:58 +0300

    feat: add Morse code decoder function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const sentence = [];
+    let words = expr.split('**********');
+    words.forEach((element) => {
+        let arr=[];
+        
+        let wordsCount = element.length / 10;
+        for (i = 0; i < wordsCount; i++) {
+            let eachLetterNums = element.slice(i * 10, (i + 1) * 10);
+
+            let letters = eachLetterNums.replace('00', '');
+            let lettersCount = letters.length / 2;
+            let result = [];
+            for (j = 0; j < lettersCount; j++) {
+                let letter = letters.slice(j * 2, (j + 1) * 2);
+                let decodedLetter;
+                switch (letter) {
+                    case '10': decodedLetter = '.';  break;
+                    case '11': decodedLetter = '-'; break;
+                } 
+                result.push(decodedLetter);
+            }
+
+            let a = result.join('');
+            let eachLetter = MORSE_TABLE[a];
+            arr.push(eachLetter);
+        }
+        
+        let eachWord = arr.join('');
+        sentence.push(eachWord);
+    });
+    return sentence.join(' '); 
 }
+
 
 module.exports = {
     decode
 }
+
+
